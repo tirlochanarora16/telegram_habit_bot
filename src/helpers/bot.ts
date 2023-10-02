58fb0823040b390ca0f5b7c36a79ecc8091736ee
tirlochanarora16 <tirlochan.arora16@gmail.com>
2023-10-02 17:30:10 +0530

    fixed redis key being created again-and-again

--- src/helpers/bot.ts
+++ src/helpers/bot.ts
@@ -24,10 +24,13 @@ export const botOnText = (
     }
 
     const redisKey = `user:${message.from?.id}`;
+    const checkKey = await redis.get(redisKey);
 
     // keeping a track of last command entered by the user with 30 min expiration.
-    await redis.set(redisKey, regex.source.slice(1));
-    await redis.expire(redisKey, 1800);
+    if (!checkKey) {
+      await redis.set(redisKey, regex.source.slice(1));
+      await redis.expire(redisKey, 1800);
+    }
 
     // if response is array, then loop through the array and send messages one-by-one.
     if (Array.isArray(callBackResponse)) {
